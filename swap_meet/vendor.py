8454d5c60dd52ca76eb967fee891642709e0f771
V <valeriya.i.ten@gmail.com>
2026-09-21 15:11:58 -0700

    implement class Vendor

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,24 @@
 class Vendor:
-    pass
+
+    def __init__(self, inventory = None):
+        if inventory is None: 
+            self.inventory = []
+        else: 
+            self.inventory = inventory
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+
+    def remove(self, item):
+        if item in self.inventory: 
+            self.inventory.remove(item)
+            return item
+        return None
+
+    def get_by_id(self, id):
+        for item in self.inventory: 
+            if item.id == id: 
+                return item
+            else: 
+                return None
