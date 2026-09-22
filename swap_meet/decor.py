bcdae19fef9988ac57ae961e7b5524837429f177
V <valeriya.i.ten@gmail.com>
2026-09-22 13:55:10 -0700

    implement class Decor in decor.py

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,2 +1,14 @@
-class Decor:
-    pass
+import uuid
+
+class Decor: 
+    def __init__(self, id=None, width=0, length=0):
+        self.id = id if id is not None else uuid.uuid4().int
+        self.width = width
+        self.length = length
+
+    def get_category(self):
+        return "Decor"
+
+    def __str__(self):
+        return f"An object of type Decor with id {id}. "\
+               f"It takes up a {self.width} by {self.length} sized space."
