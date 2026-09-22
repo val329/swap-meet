6ca227652f38447824f9228c7376de0b80d1b756
V <valeriya.i.ten@gmail.com>
2026-09-22 14:31:41 -0700

    convert item to a parent class

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,7 +1,9 @@
 import uuid
+CONDITIONS = ["very not mint condition", "visibly used", "slightly used", "mint", "as new"]
+
 class Item:
 
-    def __init__(self, id=None):
+    def __init__(self, id=None, condition=0):
         if id:  
             self.id = id
         else: 
@@ -10,5 +12,18 @@ class Item:
     def get_category(self):
         return "Item"
 
-    
+
+
+
+
+
+
+
+
+
+    def condition_description(self):
+        index = round(self.condition) - 1
+        return CONDITIONS[index]
+
+
 
