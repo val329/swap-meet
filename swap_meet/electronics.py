b0119ed6ff7d1207b4c40796da9a55afe44057df
V <valeriya.i.ten@gmail.com>
2026-09-22 14:21:21 -0700

    add condition_description function

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,9 +1,11 @@
 import uuid
+CONDITIONS = ["very not mint condition", "visibly used", "slightly used", "mint", "as new"]
 
 class Electronics: 
-    def __init__(self, id=None, type="Unknown"):
+    def __init__(self, id=None, type="Unknown", condition=0):
         self.id = id if id is not None else uuid.uuid4().int
         self.type = type
+        self.condition = condition
 
     def get_category(self):
         return "Electronics"
@@ -11,3 +13,8 @@ class Electronics:
     def __str__(self):
         return  f"An object of type Electronics with id {self.id}. "\
                 f"This is a {self.type} device."
+
+    def condition_description(self):
+        index = round(self.condition) - 1
+        return CONDITIONS[index]
+    
