aa4466a9885fbed80a385ba2f9435805812547d7
V <valeriya.i.ten@gmail.com>
2026-09-22 13:35:56 -0700

    implement class Clothing for wave 5

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,19 @@
+import uuid
 class Clothing:
-    pass
+
+    def __init__(self, id=None, fabric="Unknown"):
+        if id is not None: 
+            self.id = id
+        else: 
+            self.id = uuid.uuid4().int
+        self.fabric = fabric
+
+    def get_category(self):
+        return "Clothing"
+
+    def __str__(self):
+        return f"An object of type Clothing with id {self.id}. "\
+               f"It is made from {self.fabric} fabric."
+
+    
+    
