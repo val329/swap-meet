2312be367fcaabf2357543814ef3436e97b0aee2
V <valeriya.i.ten@gmail.com>
2026-09-22 13:56:13 -0700

    implement class Electronics in electronics.py

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,2 +1,13 @@
-class Electronics:
-    pass
+import uuid
+
+class Electronics: 
+    def __init__(self, id=None, type="Unknown"):
+        self.id = id if id is not None else uuid.uuid4().int
+        self.type = type
+
+    def get_category(self):
+        return "Electronics"
+
+    def __str__(self):
+        return  f"An object of type Electronics with id {self.id}. "\
+                f"This is a {self.type} device."
