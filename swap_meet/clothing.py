dbbc581bfc3294ebf77859a609fa22fd5a8502c2
V <valeriya.i.ten@gmail.com>
2026-09-22 13:42:53 -0700

    implement class Decor for wave 5

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -15,5 +15,19 @@ class Clothing:
         return f"An object of type Clothing with id {self.id}. "\
                f"It is made from {self.fabric} fabric."
 
-    
-    
+class Decor: 
+    def __init__(self, width=0, length=0):
+        if id is not None: 
+            self.id = id
+        else: 
+            self.id = uuid.uuid4().int
+        self.width = width
+        self.length = length
+
+    def get_category(self):
+        return "Decor"
+
+    def __str__(self):
+        return f"An object of type Decor with id {id}. "\
+               f"It takes up a {self.width} by {self.length} sized space."
+
