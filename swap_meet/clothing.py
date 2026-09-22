457fee9d4b25a62c68c2aaa3c7474d5186e81170
V <valeriya.i.ten@gmail.com>
2026-09-22 13:56:59 -0700

    fix class clothing in file clothing.py

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,11 +1,8 @@
 import uuid
-class Clothing:
 
+class Clothing:
     def __init__(self, id=None, fabric="Unknown"):
-        if id is not None: 
-            self.id = id
-        else: 
-            self.id = uuid.uuid4().int
+        self.id = id if id is not None else uuid.uuid4().int
         self.fabric = fabric
 
     def get_category(self):
@@ -14,20 +11,3 @@ class Clothing:
     def __str__(self):
         return f"An object of type Clothing with id {self.id}. "\
                f"It is made from {self.fabric} fabric."
-
-class Decor: 
-    def __init__(self, width=0, length=0):
-        if id is not None: 
-            self.id = id
-        else: 
-            self.id = uuid.uuid4().int
-        self.width = width
-        self.length = length
-
-    def get_category(self):
-        return "Decor"
-
-    def __str__(self):
-        return f"An object of type Decor with id {id}. "\
-               f"It takes up a {self.width} by {self.length} sized space."
-
