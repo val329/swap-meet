552cb67f2a3486016e72c879120fcdb8686b405f
V <valeriya.i.ten@gmail.com>
2026-09-22 14:22:01 -0700

    fix bugs in clothing and decor classes

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,9 +1,10 @@
 import uuid
 
 class Clothing:
-    def __init__(self, id=None, fabric="Unknown"):
+    def __init__(self, id=None, fabric="Unknown", condition=0):
         self.id = id if id is not None else uuid.uuid4().int
         self.fabric = fabric
+        self.condition = condition
 
     def get_category(self):
         return "Clothing"
--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,14 +1,15 @@
 import uuid
 
 class Decor: 
-    def __init__(self, id=None, width=0, length=0):
+    def __init__(self, id=None, width=0, length=0, condition=0):
         self.id = id if id is not None else uuid.uuid4().int
         self.width = width
         self.length = length
+        self.condition = condition
 
     def get_category(self):
         return "Decor"
 
     def __str__(self):
-        return f"An object of type Decor with id {id}. "\
+        return f"An object of type Decor with id {self.id}. "\
                f"It takes up a {self.width} by {self.length} sized space."
