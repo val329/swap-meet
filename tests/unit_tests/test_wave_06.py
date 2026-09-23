95f23ffb017146462feba54ebafe4eea20d0a456
V <valeriya.i.ten@gmail.com>
2026-09-23 14:55:14 -0700

    add test for wave 6

--- tests/unit_tests/test_wave_06.py
+++ tests/unit_tests/test_wave_06.py
@@ -160,15 +160,31 @@ def test_swap_best_by_category_reordered():
         their_priority="Decor"
     )
 
-    raise Exception("Complete this test according to comments below.")
+    # raise Exception("Complete this test according to comments below.")
     # *********************************************************************
     # ****** Complete Assert Portion of this test **********
     # *********************************************************************
     # Assertions should check:
     # - That result is truthy
     # - That tai and jesse's inventories are the correct length
-    # - That all the correct items are in tai and jesse's inventories, and that the items that were swapped are not there
+    # - That all the correct items are in tai and jesse's inventories, 
+    # and that the items that were swapped are not there
     
+    assert result
+    assert len(tai.inventory) == 3
+    assert len(jesse.inventory) == 3
+    assert item_a in jesse.inventory
+    assert item_b in jesse.inventory
+    assert item_f in jesse.inventory
+    assert item_d in tai.inventory
+    assert item_e in tai.inventory
+    assert item_c in tai.inventory
+    assert item_a not in tai.inventory
+    assert item_d not in jesse.inventory
+    
+
+
+
 #@pytest.mark.skip
 def test_swap_best_by_category_no_inventory_is_false():
     tai = Vendor(
