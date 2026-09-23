0009f46ccf47be51b0aba3434d9aff523b8ee0bc
Tatsiana Khurs <tatsianakhurs@gmail.com>
2026-09-23 14:38:11 -0700

    Completed Test test_swap_best_by_category_reordered

--- tests/unit_tests/test_wave_06.py
+++ tests/unit_tests/test_wave_06.py
@@ -122,7 +122,7 @@ def test_swap_best_by_category():
     # - That all the correct items are in tai and jesse's inventories, including the items which were swapped from one vendor to the other
     
     # Assert
-    
+
     assert result
 
     assert len(tai.inventory) == 3
@@ -160,7 +160,7 @@ def test_swap_best_by_category_reordered():
         their_priority="Decor"
     )
 
-    raise Exception("Complete this test according to comments below.")
+    #raise Exception("Complete this test according to comments below.")
     # *********************************************************************
     # ****** Complete Assert Portion of this test **********
     # *********************************************************************
@@ -169,6 +169,21 @@ def test_swap_best_by_category_reordered():
     # - That tai and jesse's inventories are the correct length
     # - That all the correct items are in tai and jesse's inventories, and that the items that were swapped are not there
     
+    #Assert
+
+    assert result
+
+    assert len(tai.inventory) == 3
+    assert len(jesse.inventory) == 3
+
+    assert item_f in tai.inventory
+    assert item_b in tai.inventory
+    assert item_a in tai.inventory
+
+    assert item_c in jesse.inventory
+    assert item_e in jesse.inventory
+    assert item_d in jesse.inventory
+
 #@pytest.mark.skip
 def test_swap_best_by_category_no_inventory_is_false():
     tai = Vendor(
